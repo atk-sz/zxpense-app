58fbfa8910e1a4b2dcdf47d398b083d0bb0d4521
Syed Zaid <syed.zaid525@gmail.com>
2025-06-29 18:38:58 +0530

    added prescreen form

--- src/screens/PreScreen.tsx
+++ src/screens/PreScreen.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { RootStackParamList } from '../utils/interfaces';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import ScreenView from '../utils/ScreenView';
@@ -13,6 +19,11 @@ type PreScreenProps = {
 const PreScreen: React.FC<PreScreenProps> = ({
   navigation,
 }): React.JSX.Element => {
+  const [name, setName] = React.useState({
+    firstName: '',
+    lastName: '',
+  });
+
   const goToHome = (): void => {
     navigation.navigate('Home');
   };
@@ -22,15 +33,29 @@ const PreScreen: React.FC<PreScreenProps> = ({
       <View style={styles.screenContainer}>
         <View style={styles.titleContainer}>
           <Icon name="diversity-2" size={30} color="#fff" />
-          <Text style={{ color: 'white', fontSize: 30, paddingLeft: 30 }}>
-            ZTrip Expenses
-          </Text>
+          <Text style={styles.titleText}>TripExpenz</Text>
         </View>
-        <View style={styles.formContainer}>
-          <TouchableOpacity style={styles.btn} onPress={goToHome}>
-            <Text>Submit</Text>
-          </TouchableOpacity>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>First Name</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your first name"
+            value={name.firstName}
+            onChangeText={value => setName({ ...name, firstName: value })}
+          />
         </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>Last Name</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your last name"
+            value={name.lastName}
+            onChangeText={value => setName({ ...name, lastName: value })}
+          />
+        </View>
+        <TouchableOpacity style={styles.btn} onPress={goToHome}>
+          <Text style={{ color: DarkTheme.text }}>Submit</Text>
+        </TouchableOpacity>
       </View>
     </ScreenView>
   );
@@ -43,13 +68,38 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   titleContainer: {
+    marginBottom: 50,
     flexDirection: 'row',
     alignItems: 'center',
   },
-  formContainer: {},
+  titleText: {
+    color: DarkTheme.text,
+    fontSize: 30,
+    paddingLeft: 30,
+  },
+  inputContainer: {
+    width: '80%',
+  },
+  inputLabel: {
+    color: DarkTheme.text,
+  },
+  input: {
+    backgroundColor: DarkTheme.dark,
+    borderWidth: 2,
+    borderColor: DarkTheme.grey,
+    borderRadius: 20,
+    height: 50,
+    padding: 10,
+    marginVertical: 10,
+  },
   btn: {
+    height: 50,
     backgroundColor: DarkTheme.secondary,
-    padding: 20,
+    width: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    marginTop: 20,
   },
 });
 
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -2,15 +2,21 @@ type Theme = {
   primary: string;
   secondary: string;
   text: string;
+  dark: string;
+  grey: string;
 };
 export const DarkTheme: Theme = {
   primary: '#161616',
   secondary: '#E52E2D',
   text: '#ffffff',
+  dark: '#121212',
+  grey: '#383838',
 };
 
 export const LightTheme: Theme = {
   primary: '#E52E2D',
   secondary: '#161616',
   text: '#ffffff',
+  dark: '#121212',
+  grey: '#383838',
 };
